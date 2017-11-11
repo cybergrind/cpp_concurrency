f8d9e215ee554a93e42b62365e5e2383773e3e21
Kirill Pinchuk <cybergrind@gmail.com>
2017-11-11 23:43:06 +0300

    Initial commit

---
+++ 01/conc.cpp
@@ -0,0 +1,26 @@
+// flycheck-clang-language-standard
+// clang++ -std=c++17 -pthread
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+using namespace std;
+
+void in_thread() {
+  std::cout << "I'm in thread\n";
+  for (int i=0; i<10; i++){
+    std::cout << ".";
+    this_thread::sleep_for(chrono::nanoseconds(1));
+  }
+}
+
+int main() {
+  cout << "before thread\n";
+  thread t(in_thread);
+  this_thread::sleep_for(chrono::nanoseconds(100000));
+  cout << "before join\n";
+  
+  t.join();
+  cout << "after join\n";
+  return 0;
+}
